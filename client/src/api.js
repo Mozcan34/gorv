d2925bba93bdc04b586898b9e3e2577ef182481c
MuhammetOzcan34 <muhammet.ozcann83@gmail.com>
2025-05-28 18:10:42 +0300

    GitHub Pages ve Google Apps Script entegrasyonu için yapılandırma güncellemeleri

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,6 @@
-// src/api.js
-export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwdlNKSlssGCSTMTQ6dsTP71YLxRuVK5LaTc4K5weW5YQwJ8b60ApHLfiFKAy_CBlLO/exec';
+// client/src/api.js
+
+// Lütfen bu URL'yi, Google Apps Script projenizin
+// "Deploy" -> "New deployment" ile oluşturduğunuz
+// "Anyone" (Herkes) erişimli ve '/exec' ile biten URL'si ile değiştirin.
+export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwdlNKSlssGCSTMTQ6dsTP71YLxRuVK5LaTc4K5weW5YQwJ8b60ApHLfiFKAy_CBlLO/exec'
